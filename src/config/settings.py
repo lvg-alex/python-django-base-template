8dbc27762f3800ea8442867236bc71b3c0cffe68
lvg-alex <54937245+lvg-alex@users.noreply.github.com>
2020-12-07 10:56:17 +0400

    Update settings.py
    To Russify the interface.

--- src/config/settings.py
+++ src/config/settings.py
@@ -95,7 +95,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
